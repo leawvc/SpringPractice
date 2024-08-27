f2aa4e807c3fb8d9e216802d3a1a51ad3427e221
igwangmin <eoeo777755@naver.com>
2024-08-27 20:22:39 +0900

    commit : Spring Context 선언 및 Bean 객체 생성 관리

---
+++ src/main/java/com/udamy/springpractice/App01GamingBasicJava.java
@@ -0,0 +1,19 @@
+package com.udamy.springpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App01GamingBasicJava{
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPracticeApplication.class, args);
+
+
+//        var game = new MarioGame();
+        var game = new PacmanGame();
+//        var game = new SuperContraGame();
+        var gameRunner = new GameRunner(game);
+        gameRunner.run();
+    }
+}
+
